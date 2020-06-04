4e8d2d9c6b1385c75bd8b0c331f081625509ae80
Dondig00 <Dondig00@estudiantes.unileon.es>
2020-06-04 18:37:27 +0200

    Toolbar vuetify

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
+
+<v-app>
+  <v-app-bar app color="red darken-3" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -23,6 +24,17 @@
 
       <v-spacer></v-spacer>
 
+      <v-toolbar-items class="hidden-xs-only" color="red darken-3">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
@@ -31,26 +43,45 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
+
     </v-app-bar>
 
-    <v-content>
+   <v-content>
       <router-view />
     </v-content>
-  </v-app>
+</v-app>
+    
+ 
 </template>
+
 
 <script>
 //import HelloWorld from "./components/HelloWorld.vue";
-
+/*
 export default {
   name: "App",
 
   components: {
     //HelloWorld
+    
   },
 
   data: () => ({
     //
   })
+};*/
+
+export default {
+  name: "App",
+  data(){
+    return {
+      sidebar: false,
+      menuItems: [
+          { title: 'Home', path: '/' },
+          { title: 'Registrarse', path: '/registrarse' },
+          { title: 'Login', path: '/login' }
+     ]
+    }
+  },
 };
 </script>
